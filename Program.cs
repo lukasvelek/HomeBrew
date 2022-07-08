5d76bc1d93b701bbcd4587adac448bb619a3dd60
Lukas Velek <lukas.velek@centrum.cz>
2022-07-08 19:11:03 +0200

    Initial commit

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using HomeBrew.Core;
+
 namespace HomeBrew
 {
     internal class Program
     {
+        static Game game;
+
         static void Main(string[] args)
         {
         }
